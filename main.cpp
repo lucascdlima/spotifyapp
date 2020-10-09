b47c9cb8534d6d7de987a9e603bddbe8bbf0ff44
lucascdlima <lucascdlima@gmail.com>
2020-10-09 01:58:50 -0300

    Removes not used objects.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "trackswidget.h"
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    TracksWidget dialog;
-    dialog.show();
 
     return a.exec();
 }
